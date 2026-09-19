e40b407f61497740d2ec185a123c7f1e5ab9da9a
Wizard <abhinavkbhanu@gmail.com>
2026-09-19 21:54:46 +0530

    Add RB22 startup exception handling

--- RB22/App.xaml.cs
+++ RB22/App.xaml.cs
@@ -1,3 +1,38 @@
+using System;
 using System.Windows;
+using System.Windows.Threading;
+
 namespace RB22;
-public partial class App : Application { }
+
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        try
+        {
+            base.OnStartup(e);
+            var window = new MainWindow();
+            MainWindow = window;
+            window.Show();
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(
+                "RB22 could not start.\n\n" + ex.GetType().Name + ": " + ex.Message,
+                "RB22 startup error",
+                MessageBoxButton.OK,
+                MessageBoxImage.Error);
+            Shutdown(1);
+        }
+    }
+
+    protected override void OnDispatcherUnhandledException(DispatcherUnhandledExceptionEventArgs e)
+    {
+        MessageBox.Show(
+            "RB22 encountered an error.\n\n" + e.Exception.GetType().Name + ": " + e.Exception.Message,
+            "RB22 error",
+            MessageBoxButton.OK,
+            MessageBoxImage.Error);
+        e.Handled = true;
+    }
+}
